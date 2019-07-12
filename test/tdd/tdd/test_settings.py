d8b8bbb3f24dde5e7b6de9fb291dde30f49d1a9d
Seemaiw <tseemaiw@gmail.com>
2019-07-12 14:05:34 +0545

    [add][.gitignore file added]

---
+++ test/tdd/tdd/test_settings.py
@@ -0,0 +1,11 @@
+# using SQLITE database for testing
+
+from .settings import *
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": ":memory:",
+    }
+}
+EMAIL_BACKEND = 'django.core.mail.backends.locmen.EmailBackend'
